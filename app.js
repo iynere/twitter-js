4e7a72ecee1b929331a009238e2530fb61e72825
isar0se <itsr053@gmail.com>
2017-01-17 15:18:34 -0500

    pretty done

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
+const socketio = require('socket.io');
 const express = require( 'express' );
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
 const routes = require('./routes/');
 const bodyParser = require('body-parser');
 const app = express(); // creates an instance of an express application
-
-app.listen(3000, function() {
+const server = app.listen(3000, function() {
 	console.log('the server started');
 });
+const io = socketio.listen(server);
 
 //MIDDLEWARE
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(volleyball);
-app.use('/', routes);
+app.use('/', routes(io));
 app.use(express.static('public'));
 
 //NUNJUCKS HTML RENDERER
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,40 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const router = express.Router();
-const tweetBank = require('../tweetBank');
+module.exports = function(io) {
+	const bodyParser = require('body-parser');
+	const express = require('express');
+	const router = express.Router();
+	const tweetBank = require('../tweetBank');
 
-router.get('/', function(req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets, showForm: true } );
-});
-
-router.get('/users/:name', function(req, res) {
-	var name = req.params.name;
-	let tweets = tweetBank.find(function(tweeter) {
-		return tweeter.name.toLowerCase().includes(name.toLowerCase());
+	router.get('/', function(req, res) {
+		let tweets = tweetBank.list();
+		res.render( 'index', { tweets: tweets, homepage: true } );
 	});
-	res.render('index', {tweets: tweets, showForm: true, userPage: true });
-});
 
-router.get('/tweets/:id', function(req, res) {
-	var id = req.params.id;
-	let tweets = tweetBank.find(function(tweeter) {
-		return tweeter.id === parseInt(id);
+	router.get('/users/:name', function(req, res) {
+		var name = req.params.name;
+		let tweets = tweetBank.find(function(tweeter) {
+			return tweeter.name.toLowerCase() === name.toLowerCase();
+		});
+		res.render('index', {tweets: tweets, userPage: true });
 	});
-	// console.log(tweets);
-	res.render('index', {tweets: tweets});
-});
 
-router.post('/tweets', function(req, res) {
-  var name = req.body.name;
-  var text = req.body.text;
-  tweetBank.add(name, text);
-  res.redirect('/');
-});
+	router.get('/tweet/;id', function(req, res) {
+		var id = req.params.id;
+		let tweets = tweetBank.find(function(tweeter) {
+			return tweeter.id === parseInt(id);
+		});
+		// console.log(tweets);
+		res.render('index', {tweets: tweets});
+	});
 
-module.exports = router;
+	router.post('/tweets', function(req, res) {
+		var name = req.body.name;
+		var text = req.body.text;
+		tweetBank.add(name, text);
+		res.redirect('/');
+	});
+
+	return router;
+}
+
+// old version before socket.io:
+// module.exports = router;
