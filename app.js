c5fd5c842514a187251c2c53597d2af1c3ae37df
Chloe Hwang <c.chloeh@gmail.com>
2017-01-17 11:14:36 -0500

    halfway there

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 const express = require( 'express' );
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
+const routes = require('./routes/');
 const app = express(); // creates an instance of an express application
-const locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-
-nunjucks.configure('views', { noCache: true });
-nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
-});
-app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 
 app.listen(3000, function() {
 	console.log('the server started');
 });
 
+//MIDDLEWARE
 app.use(volleyball);
+app.use('/', routes);
+app.use(express.static('public'));
 
 // app.use('/', function (req, res, next) {
 // 	console.log("logged request: ", req.method, req.path, res.statusCode);
@@ -35,11 +23,25 @@ app.use(volleyball);
 // 	next();
 // });
 
-app.get('/', function(req, res) {
-	res.send('hello & welcome');
-});
 
-res.render( 'index', {title: 'Hall of Fame', people: people} )
+// app.get('/', function(req, res, next){
+//     res.render( 'index', {title: 'All About Me', people: people} );
+// })
+
+
+//ROUTES
+
+
+
+//NUNJUCKS HTML RENDERER
+const people = [{name: 'Grace'}, {name: 'Hopper'}, {name: 'Daughter'}];
+nunjucks.configure("views", { noCache: true })
+app.set('view engine', 'html'); // have res.render work with html files. This is middleware
+app.engine('html', nunjucks.render)
+
+
+
+
 
 // var express = require('express')
 // var app = express()
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+// could use one line instead: const router = require('express').Router();
+const tweetBank = require('../tweetBank');
+
+router.get('/', function (req, res) {
+  let tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+// router.get('/stylesheets/style.css', function(req, res, next) {
+//   res.sendFile('/stylesheets/style.css');
+// })
+
+module.exports = router;
